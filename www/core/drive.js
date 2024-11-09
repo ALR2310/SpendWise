5f91d217bb9212214ca45278cbd30b5bab35455e
ALR2310 <anlerip@gmail.com>
2024-11-09 23:45:27 +0700

    Thêm chức năng tải lên, tải xuống tệp tin cho Drive

--- www/core/drive.js
+++ www/core/drive.js
@@ -32,10 +32,68 @@ const logout = async () => {
     }
 };
 
+const uploadFile = async ({ fileName, mimeType, data }) => {
+    if (!accessToken) return { success: false, message: 'not logged in' };
+
+    const url = "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart";
+    const metadata = { name: fileName, mimeType: mimeType, };
+
+    const form = new FormData();
+    form.append("metadata", new Blob([JSON.stringify(metadata)], { type: "application/json" }));
+    form.append("file", new Blob([data], { type: mimeType }));
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: new Headers({
+                Authorization: `Bearer ${accessToken}`,
+            }),
+            body: form,
+        });
+
+        const responseData = await response.json();
+        return { success: true, message: "Tải lên tệp tin thành công", data: responseData };
+    } catch (error) {
+        console.error("Lỗi không xác định:", error);
+        return { success: false, message: "Có lỗi khi tải lên tệp tin", error: error };
+    }
+};
+
+const downloadFile = async (fileId) => {
+    if (!accessToken) return { success: false, message: 'not logged in' };
+
+    const url = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`;
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: new Headers({
+                Authorization: `Bearer ${accessToken}`,
+            }),
+        });
+
+        if (response.ok) {
+            const data = await response.blob();
+            // console.log(JSON.parse(await data.text()))
+            return { success: true, message: "Tải xuống tệp tin thành công", data: data };
+        } else {
+            const errorData = await response.json();
+            console.error("Lỗi khi tải tệp:", errorData);
+            return { success: false, message: "Có lỗi khi tải xuống tệp tin", error: errorData };
+        }
+    } catch (error) {
+        console.error("Lỗi không xác định:", error);
+        return { success: true, message: "Có lỗi khi tải xuống tệp tin", error: error };
+    }
+};
+
+
 export default {
     getAccessToken: () => accessToken,
     setAccessToken: (token) => accessToken = token,
     checkLogin,
     login,
     logout,
+    uploadFile,
+    downloadFile,
 };
